36575c176032e90eacee4a91bb1e32dcbb69b25e
Helen Chong <contact@helenchong.dev>
2025-12-30 10:40:55 +0800

    refactor: convert opening spaces to tabs

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,26 +1,26 @@
 import { DateTime } from 'luxon';
 
 export default function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("./src/assets/");
-    eleventyConfig.addWatchTarget("./src/assets/");
-    eleventyConfig.addPassthroughCopy({
-        "./src/assets/favicon/favicon.ico": "/favicon.ico"
-    });
+	eleventyConfig.addPassthroughCopy("./src/assets/");
+	eleventyConfig.addWatchTarget("./src/assets/");
+	eleventyConfig.addPassthroughCopy({
+		"./src/assets/favicon/favicon.ico": "/favicon.ico"
+	});
 
-    eleventyConfig.addShortcode("currentYear", () => `${new Date().getFullYear()}`);
+	eleventyConfig.addShortcode("currentYear", () => `${new Date().getFullYear()}`);
 
-    eleventyConfig.addFilter("dateToFormat", (date, format) => {
-        return DateTime.fromJSDate(date, { zone: 'utc' }).toFormat(
-            String(format)
-        )
-    });
+	eleventyConfig.addFilter("dateToFormat", (date, format) => {
+		return DateTime.fromJSDate(date, { zone: 'utc' }).toFormat(
+			String(format)
+		)
+	});
 
-    return {
-        markdownTemplateEngine: "njk",
-        htmlTemplateEngine: "njk",
-        dir: {
-            input: "src"
-        },
-        pathPrefix: "/resume/",
-    };
+	return {
+		markdownTemplateEngine: "njk",
+		htmlTemplateEngine: "njk",
+		dir: {
+			input: "src"
+		},
+		pathPrefix: "/resume/",
+	};
 };
