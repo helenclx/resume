634e7abc0bc244a57a7c6964ecc9a3dd731b7367
Helen Chong <contact@helenchong.dev>
2026-07-18 16:07:27 +0800

    all: fix code according to Biome linter suggestions

--- src/_data/sitemeta.js
+++ src/_data/sitemeta.js
@@ -1,5 +1,5 @@
 export const url = "https://helenchong.dev/resume";
-export const canonical = url + "/";
+export const canonical = `${url}/`;
 export const lang = "en";
 export const locale = "en_MY";
 export const title = "Résumé of Helen Chong";
